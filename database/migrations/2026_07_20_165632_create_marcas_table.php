eac64539c025b0f45aa8e1696dfcb3784bec82cc
Sérgio Silva <sergiolimasilva12@gmail.com>
2026-07-20 14:21:12 -0300

    sim 2

--- database/migrations/2026_07_20_165632_create_marcas_table.php
+++ database/migrations/2026_07_20_165632_create_marcas_table.php
@@ -4,15 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('marcas', function (Blueprint $table) {
             $table->id();
+            $table->string('nome', 30)->unique();
+            $table->string('imagem', 100)->comment('Logo da marca');
             $table->timestamps();
         });
     }
--- database/migrations/2026_07_20_170302_create_clientes_table.php
+++ database/migrations/2026_07_20_170302_create_clientes_table.php
@@ -4,15 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
+            $table->string('nome', 30);
             $table->timestamps();
         });
     }
