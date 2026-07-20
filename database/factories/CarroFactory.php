81a53e2f6086860d884e18bd85cc888f71c9e25e
Sérgio Silva <sergiolimasilva12@gmail.com>
2026-07-20 14:03:33 -0300

    Sim

---
+++ database/factories/CarroFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Carro;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Carro>
+ */
+class CarroFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
