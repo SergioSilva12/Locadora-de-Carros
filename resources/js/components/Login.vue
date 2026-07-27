84a1d6beea026bec52717d4f37d53b611854b12b
Sérgio Silva <sergiolimasilva12@gmail.com>
2026-07-27 14:45:54 -0300

    Criando o dropdown do nav

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,32 +1,28 @@
 <template>
     <div class="container">
-
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">Login</div>
-
                     <div class="card-body">
-                        <form method="POST" action="">
-
+                        <form method="POST" action="" @submit.prevent="login($event)">
+                            <input type="hidden" name="_token" :value="token_csrf">
                             <div class="row mb-3">
-                                <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
+                                <label for="email" class="col-md-4 col-form-label text-md-end">Email:</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email"
-                                        class="form-control" name="email"
-                                        value="" required autocomplete="email" autofocus>
+                                    <input id="email" type="email" class="form-control" name="email" value="" required
+                                        autocomplete="email" autofocus v-model="email">
 
                                 </div>
                             </div>
 
                             <div class="row mb-3">
-                                <label for="password" class="col-md-4 col-form-label text-md-end">Senha</label>
+                                <label for="password" class="col-md-4 col-form-label text-md-end">Senha:</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password"
-                                        class="form-control" name="password"
-                                        required autocomplete="current-password">
+                                    <input id="password" type="password" class="form-control" name="password" required
+                                        autocomplete="current-password" v-model="password">
 
                                 </div>
                             </div>
@@ -49,11 +45,11 @@
                                         Login
                                     </button>
 
-                                    
+
                                     <a class="btn btn-link" href="">
                                         Esqueci a senha
                                     </a>
-                                    
+
                                 </div>
                             </div>
                         </form>
@@ -65,5 +61,34 @@
 </template>
 
 <script>
+export default {
+    props: ['token_csrf'],//funcionamento semelhante ao data
+    data() {
+        return {
+            email: '',
+            password: '',
+        }
+    },
+    methods: {
+        login(e) {
+            let url = 'http://127.0.0.1:8000/api/login'
+            let configuracao = {
+                method: 'post',
+                body: new URLSearchParams({
+                    'email': this.email,
+                    'password': this.password,
+                })
+            }
+            fetch(url, configuracao)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.token) {
+                        document.cookie = 'token=' + data.token;
+                    }
+                    e.target.submit();
+                });
 
+        }
+    }
+} 
 </script>
