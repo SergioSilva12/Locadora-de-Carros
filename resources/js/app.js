98a1430e7bdbeedd8fe02b0e0a576737d8d63047
Sérgio Silva <sergiolimasilva12@gmail.com>
2026-07-28 15:25:23 -0300

    Criando o componente da table de marcas.vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,12 +20,14 @@ import LoginComponent from './components/Login.vue';
 import HomeComponent from './components/Home.vue';
 import MarcasComponent from './components/Marcas.vue';
 import InputContainer from './components/InputContainer.vue';
+import TableComponent from './components/Table.vue';
 
 app.component('example-component', ExampleComponent);
 app.component('login-component', LoginComponent);
 app.component('home-component', HomeComponent);
 app.component('marca-component',MarcasComponent);
 app.component('input-container-component', InputContainer);
+app.component('table-component',TableComponent);
 
 /**
  * The following block of code may be used to automatically register your
--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -38,39 +38,9 @@
                 <!-- INICIO DO CARD DE LISTAGEM DE MARCAS-->
                 <div class="card mb-3">
                     <div class="card-header">Relações de marcas</div>
-
-                    <!-- IINICIO DO CARD DE BUSCA -->
+                    <table-component></table-component>
                     <div class="card-body">
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">First</th>
-                                    <th scope="col">Last</th>
-                                    <th scope="col">Handle</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                    <td>@twitter</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        
                     </div>
 
                     <div class="card-footer">
